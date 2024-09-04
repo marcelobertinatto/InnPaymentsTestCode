03f23580f5c82b60c9d7159b557e5517b4727593
Marcelo Bertinatto <marcelobertinatto@hotmail.com>
2024-09-04 00:28:11 -0300

    fixed the project folder structure  added swagger did a first test adding getting all products and also added the first angular project

---
+++ 0 - Presentation/InCommPaymentWebApiCore/Controller/ProductController.cs
@@ -0,0 +1,162 @@
+﻿using InCommPayment.Application.Interfaces;
+using InCommPayment.Domain.Model;
+using InCommPaymentWebApiCore.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InCommPaymentWebApiCore.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public readonly IAppServiceProduct _appServiceProduct;
+        public ProductController(IAppServiceProduct appServiceProduct)
+        {
+            _appServiceProduct = appServiceProduct;
+        }
+
+        [HttpGet]
+        [Route("getproductbyid/{productId}")]
+        public async Task<Response<Product>> GetProductById(string productId)
+        {
+            try
+            {
+                var listProducts = new List<Product>();
+                var result = await _appServiceProduct.Get(productId);
+
+                if (result == null)
+                {
+                    return new Response<Product>()
+                    {
+                        message = "product not found",
+                        returnType = ReturnType.NotFound
+                    };
+                }
+
+                listProducts.Add(result);
+
+                return new Response<Product>()
+                {
+                    message = "product found",
+                    returnType = ReturnType.NotFound,
+                    returnedValue = listProducts
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response<Product>()
+                {
+                    message = ex.Message,
+                    returnType = ReturnType.InternalError
+                };
+            }
+        }
+
+        [HttpGet]
+        [Route("getallproducts")]
+        public async Task<Response<Product>> GetAllProducts()
+        {
+            try
+            {
+                var result = await _appServiceProduct.GetAll();
+
+                if (result == null)
+                {
+                    return new Response<Product>()
+                    {
+                        message = "products list is empty",
+                        returnType = ReturnType.NotFound
+                    };
+                }
+
+                return new Response<Product>()
+                {
+                    message = "products found",
+                    returnType = ReturnType.Success,
+                    returnedValue = result
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response<Product>()
+                {
+                    message = ex.Message,
+                    returnType = ReturnType.InternalError
+                };
+            }
+        }
+
+        [HttpPost]
+        [Route("addallproducts")]
+        public async Task<Response<Product>> AddAllProducts(List<Product>? listProducts)
+        {
+            try
+            {
+                //List<Product>? listProducts = null;
+                if(listProducts.Count == 0)
+                {
+                    listProducts = new List<Product>()
+                    {
+                        new Product()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name = "Product 1",
+                            Description = "Description Product 1",
+                            Price = 100,
+                            Quantity = 10
+                        },
+                        new Product()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name = "Product 2",
+                            Description = "Description Product 2",
+                            Price = 50,
+                            Quantity = 100
+                        },
+                        new Product()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name = "Product 3",
+                            Description = "Description Product 3",
+                            Price = 99,
+                            Quantity = 10
+                        },
+                        new Product()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name = "Product 4",
+                            Description = "Description Product 4",
+                            Price = 150,
+                            Quantity = 40
+                        },
+
+                    };
+
+                    _appServiceProduct.AddAll(listProducts);
+                    return new Response<Product>()
+                    {
+                        message = "Added successfully",
+                        returnType = ReturnType.Success
+                    };
+                }
+                else
+                {
+                    _appServiceProduct.AddAll(listProducts);
+                    return new Response<Product>()
+                    {
+                        message = "Added successfully",
+                        returnType = ReturnType.Success
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new Response<Product>()
+                {
+                    message = ex.Message,
+                    returnType = ReturnType.InternalError
+                };
+            }
+        }
+    }
+}
---
+++ 0 - Presentation/InCommPaymentWebApiCore/Program.cs
@@ -0,0 +1,66 @@
+using InCommPayment.Application.Interfaces;
+using InCommPayment.Application.Services;
+using InCommPayment.Domain.Interfaces;
+using InCommPayment.Domain.Interfaces.Repositories;
+using InCommPayment.Domain.Interfaces.Services;
+using InCommPayment.Domain.Services;
+using InCommPayment.Infra.Context;
+using InCommPayment.Infra.Repository;
+using InCommPayment.Infra.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace InCommPaymentWebApiCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            //Db
+            builder.Services.AddDbContext<DBContext>(opt => opt.UseInMemoryDatabase("ProductOrder"));
+
+            //DI mapping
+
+            //application
+            builder.Services.AddTransient(typeof(IAppServiceBase<>), typeof(AppServiceBase<>));
+            builder.Services.AddTransient(typeof(IAppServiceOrder), typeof(AppServiceOrder));
+            builder.Services.AddTransient(typeof(IAppServiceProduct), typeof(AppServiceProduct));
+
+            //domain
+            //services
+            builder.Services.AddTransient(typeof(IServiceBase<>), typeof(ServiceBase<>));
+            builder.Services.AddTransient(typeof(IServiceOrder), typeof(ServiceOrder));
+            builder.Services.AddTransient(typeof(IServiceProduct), typeof(ServiceProduct));
+
+            //repository
+            builder.Services.AddTransient(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+            builder.Services.AddTransient(typeof(IOrderRepository), typeof(RepositoryOrder));
+            builder.Services.AddTransient(typeof(IProductRepository), typeof(RepositoryProduct));
+
+            //infra
+            //unitOfWork
+            builder.Services.AddTransient(typeof(IUnitOfWork), typeof(UnitOfWork));
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            var app = builder.Build();
+
+            app.MapControllers();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            //app.UseSwaggerUI(c =>
+            //{
+            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My service");
+            //    c.RoutePrefix = string.Empty; 
+            //});
+
+            app.Run();
+        }
+    }
+}
